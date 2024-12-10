c5674377a8b26fc402e44d20dfcb675a5e4b3d25
FernandoDlas <105080873+FernandoDlas@users.noreply.github.com>
2024-12-10 09:28:45 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 documento.getElementByld('formulario').addEventListener('submit,function')(event){
       event.preventDefault();
-}
+      // Obter as notas inseridas pelo usuário 
+      let nota1= parseFloat(document.getElementByld('nota1').value);
+      let nota2= parseFloat(document.getElementById.('nota2').value);
+      let nota3= parseFRloat(document.getElementByld('nota3').value);
+
+      //Calcular a média
+      let media=(nota1+nota3) /3;
+
+      //Exibir a mensagem de resultado
+      let resultadoDiv= document.getElementByld('resultaso')
+      if (mesia>=6.0)}
+      resultadoDiv.innerHTML=<p class="aprovado">aprovado!Sua média foi
